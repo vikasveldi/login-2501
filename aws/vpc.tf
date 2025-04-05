08de40eb2f0430fddc9c233ab5d24a13a3152bae
ravi2krishna <ravi2krishna@gmail.com>
2025-04-05 12:55:08 +0530

    update script

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -288,6 +288,7 @@ resource "aws_instance" "lms-web-server" {
                     #!/bin/bash
                     
                     # Setup App 
+                    sudo apt update -y
                     sudo apt install nginx -y
                     sudo rm -rf /var/www/html
                     sudo git clone https://github.com/ravi2krishna/login-2501.git /var/www/html
