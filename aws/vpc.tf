498b442b6d51a047ee807708abf4e6453cb5bbca
ravi2krishna <ravi2krishna@gmail.com>
2025-04-05 12:06:20 +0530

    create api sg - http rule

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -238,3 +238,12 @@ resource "aws_vpc_security_group_ingress_rule" "lms-api-sg-ssh" {
   ip_protocol       = "tcp"
   to_port           = 22
 }
+
+# Security Group Rule - HTTP
+resource "aws_vpc_security_group_ingress_rule" "lms-api-sg-http" {
+  security_group_id = aws_security_group.lms-api-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
