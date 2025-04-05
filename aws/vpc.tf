54a07ad76ab58ebb129dde6da238d91a0751d07a
ravi2krishna <ravi2krishna@gmail.com>
2025-04-05 11:55:08 +0530

    create api nacl

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -123,3 +123,30 @@ resource "aws_network_acl_association" "lms-web-nacl-asc" {
   network_acl_id = aws_network_acl.lms-web-nacl.id
   subnet_id      = aws_subnet.lms-web-sn.id
 }
+
+# NACL - API Subnet
+resource "aws_network_acl" "lms-api-nacl" {
+  vpc_id = aws_vpc.lms-vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "lms-api-nacl"
+  }
+}
