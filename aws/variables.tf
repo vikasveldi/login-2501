29246289071b8315be9b748e449795fa76b68392
ravi2krishna <ravi2krishna@gmail.com>
2025-04-05 11:20:13 +0530

    aws crdentials as variables

---
+++ aws/variables.tf
@@ -0,0 +1,11 @@
+# AWS Access Key
+variable aws_access_key {
+  type        = string
+  description = "Input AWS Access Key"
+}
+
+# AWS Secret Key
+variable aws_secret_key {
+  type        = string
+  description = "Input AWS Secret Key"
+}
