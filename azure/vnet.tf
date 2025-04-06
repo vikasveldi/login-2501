8b678b4490aab46f8359838952f8e7707e307cb3
ravi2krishna <ravi2krishna@gmail.com>
2025-04-06 11:59:00 +0530

    create DB ssh rule

--- azure/vnet.tf
+++ azure/vnet.tf
@@ -140,3 +140,18 @@ resource "azurerm_network_security_group" "lms-db-nsg" {
   location            = azurerm_resource_group.lms-rg.location
   resource_group_name = azurerm_resource_group.lms-rg.name
 }
+
+# DB NSG SSH Rule
+resource "azurerm_network_security_rule" "lms-db-nsg-ssh" {
+  name                        = "lms-db-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.lms-rg.name
+  network_security_group_name = azurerm_network_security_group.lms-db-nsg.name
+}
