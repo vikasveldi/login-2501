afe0edd3dfcdc472a60e6d39656d8372c6e4a9d4
ravi2krishna <ravi2krishna@gmail.com>
2025-04-06 11:51:03 +0530

    create web ssh rule

--- azure/vnet.tf
+++ azure/vnet.tf
@@ -66,3 +66,18 @@ resource "azurerm_network_security_group" "lms-web-nsg" {
   location            = azurerm_resource_group.lms-rg.location
   resource_group_name = azurerm_resource_group.lms-rg.name
 }
+
+# WEB NSG SSH Rule
+resource "azurerm_network_security_rule" "lms-web-nsg-ssh" {
+  name                        = "lms-web-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.lms-rg.name
+  network_security_group_name = azurerm_network_security_group.lms-web-nsg.name
+}
