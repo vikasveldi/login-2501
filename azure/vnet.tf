805c3f26cf6b7ed4fc9d37eced93019fa113d39c
ravi2krishna <ravi2krishna@gmail.com>
2025-04-06 12:21:15 +0530

    create lms web server

--- azure/vnet.tf
+++ azure/vnet.tf
@@ -229,3 +229,33 @@ resource "azurerm_network_interface_security_group_association" "lms-db-nic-nsg"
   network_interface_id          = azurerm_network_interface.lms-db-nic.id
   network_security_group_id     = azurerm_network_security_group.lms-db-nsg.id
 }
+
+# LMS WEB SERVER
+resource "azurerm_linux_virtual_machine" "lms-web-server" {
+  name                = "lms-web-server"
+  resource_group_name = azurerm_resource_group.lms-rg.name
+  location            = azurerm_resource_group.lms-rg.location
+  size                = "Standard_F2"
+  admin_username      = "ubuntu"
+  custom_data         = filebase64("script.sh")
+  network_interface_ids = [
+    azurerm_network_interface.lms-web-nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "ubuntu"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
