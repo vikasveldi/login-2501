e4d4fd1b0ed77b465551df2d58eb3170d7c864fb
ravi2krishna <ravi2krishna@gmail.com>
2025-04-06 12:22:00 +0530

    deployment script

---
+++ azure/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Updating the System"
+sudo apt update -y
+
+echo "Installing Utilities"
+sudo apt install zip unzip -y 
+
+echo "Installing NGINX Web Server"
+sudo apt install nginx -y
+
+echo "Cleanup Document Root"
+sudo rm -rf /var/www/html
+
+echo "Clone Login App"
+sudo git clone https://github.com/ravi2krishna/login-2501.git /var/www/html
+
+echo "Finished Deployment Process"
